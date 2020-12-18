450de250ab399c1036a4ed207f8053d5f30c9a2a
Marcus Lim <marcz68@gmail.com>
2020-12-18 11:18:28 +0800

    added show error for log in screen

--- App.js
+++ App.js
@@ -9,7 +9,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Chat" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
